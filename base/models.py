e78016284c79320708354f44f3c710c2593b5204
ahmed-gaber-elgamal <yakan44444@gmail.com>
2020-11-11 00:00:15 +0200

    added slug and replace post.id

--- base/models.py
+++ base/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from  django.utils.text import slugify
 
 class Tag(models.Model):
     name = models.CharField(max_length=200)
@@ -17,7 +17,19 @@ class Post(models.Model):
     active = models.BooleanField(default=False)
     featured = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, null=True)
-    #slug
+    slug = models.SlugField(null=True, blank=True)
 
     def __str__(self):
         return self.headline
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            slug = slugify(self.headline)
+            has_slug = Post.objects.filter(slug=slug).exists()
+            count = 1
+            while has_slug:
+                count += 1
+                slug = slugify(self.headline) + '-' + str(count)
+                has_slug = Post.objects.filter(slug=slug).exists()
+            self.slug = slug
+        super().save(*args, **kwargs)
--- base/views.py
+++ base/views.py
@@ -32,8 +32,8 @@ def posts(request):
     return render(request, 'base/posts.html', context)
 
 
-def post(request, pk):
-    post = Post.objects.get(id=pk)
+def post(request, slug):
+    post = Post.objects.get(slug=slug)
     context = {'post': post}
     return render(request, 'base/post.html', context)
 
@@ -55,8 +55,8 @@ def createPost(request):
 
 
 @login_required(login_url='home')
-def updatePost(request, pk):
-    post = Post.objects.get(id=pk)
+def updatePost(request, slug):
+    post = Post.objects.get(slug=slug)
     form = PostForm(instance=post)
 
     if request.method == 'POST':
@@ -69,8 +69,8 @@ def updatePost(request, pk):
 
 
 @login_required(login_url='home')
-def deletePost(request, pk):
-    post = Post.objects.get(id=pk)
+def deletePost(request, slug):
+    post = Post.objects.get(slug=slug)
     if request.method == 'POST':
         post.delete()
         return redirect('posts')
